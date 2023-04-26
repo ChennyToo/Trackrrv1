d3060d439f8039218f497e55c1839d1195286dfd
ChennyToo <kchencd1@gmail.com>
2023-04-26 10:38:09 -0400

    git commit

--- app/src/main/java/com/example/trackrrv1/MainFragment.kt
+++ app/src/main/java/com/example/trackrrv1/MainFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.example.trackrrv1.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -29,6 +32,16 @@ class MainFragment : Fragment() {
 
         })
         // Inflate the layout for this fragment
+        val buttonsClickListener: View.OnClickListener =
+            View.OnClickListener { view ->
+                when(view.id){
+                    R.id.NewFoodButton -> {
+                        binding.root.findNavController().navigate(R.id.action_mainFragment_to_cameraFragment)
+                    }
+                }
+            }
+        binding.NewFoodButton.setOnClickListener(buttonsClickListener)
+
 
         return binding.root    }
 
