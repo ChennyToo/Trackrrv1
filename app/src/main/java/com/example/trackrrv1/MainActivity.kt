589e0c1135fb2da35142c86b7ac81834cdf7c010
ChennyToo <kchencd1@gmail.com>
2023-05-27 22:56:54 -0400

    -git commit

--- app/src/main/java/com/example/trackrrv1/MainActivity.kt
+++ app/src/main/java/com/example/trackrrv1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.trackrrv1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.lifecycle.lifecycleScope
@@ -24,8 +25,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+ 
+
+
+
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION,)
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION,)
+
+
+
 
 
 
